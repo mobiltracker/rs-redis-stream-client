24cf57d3ad112269892963951ef334c377aeeebe
Matheus Cruz <mlcruz@inf.ufrgs.br>
2021-11-03 20:34:44 -0300

    working redis stream client

--- src/lib.rs
+++ src/lib.rs
@@ -1,31 +1,43 @@
 use std::collections::HashMap;
 
 use redis::{
-    streams::{StreamReadOptions, StreamReadReply},
+    aio::MultiplexedConnection,
+    streams::{StreamInfoGroupsReply, StreamReadOptions, StreamReadReply},
     AsyncCommands, Value,
 };
 
-pub struct RedisStreamClient<'a, 'b, T: AsyncCommands> {
-    connection: &'a mut T,
-    consumer_group: &'b str,
-    stream_key: &'b str,
+pub struct RedisStreamClient {
+    connection: MultiplexedConnection,
+    consumer_group: &'static str,
+    stream_key: &'static str,
     consumer_key: String,
     options: StreamReadOptions,
 }
 
-pub struct RedisStreamMessage<'a, T: AsyncCommands> {
+pub struct RedisStreamMessage {
     key: String,
-    stream_key: &'a str,
-    group_key: &'a str,
+    stream_key: &'static str,
+    group_key: &'static str,
     inner_map: HashMap<String, Value>,
-    connection: &'a mut T,
+    connection: MultiplexedConnection,
 }
 
-impl<'a, T: AsyncCommands> RedisStreamMessage<'a, T> {
+impl std::fmt::Debug for RedisStreamMessage {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("RedisStreamMessage")
+            .field("key", &self.key)
+            .field("stream_key", &self.stream_key)
+            .field("group_key", &self.group_key)
+            .field("inner_map", &self.inner_map)
+            .finish()
+    }
+}
+
+impl RedisStreamMessage {
     pub fn get_field(&self, k: &str) -> Option<&Value> {
         self.inner_map.get(k)
     }
-    pub async fn ack(self) -> Result<(), redis::RedisError> {
+    pub async fn ack(mut self) -> Result<(), redis::RedisError> {
         let _: () = self
             .connection
             .xack(self.stream_key, self.group_key, &[self.key])
@@ -35,18 +47,25 @@ impl<'a, T: AsyncCommands> RedisStreamMessage<'a, T> {
     }
 }
 
-impl<'a, 'b, T: AsyncCommands> RedisStreamClient<'a, 'b, T> {
+impl RedisStreamClient {
     pub async fn new(
-        connection: &'a mut T,
-        stream_key: &'b str,
-        consumer_group: &'b str,
-        consumer_prefix: &'b str,
-    ) -> Result<RedisStreamClient<'a, 'b, T>, redis::RedisError> {
-        // We do not care if this fails (its going to fail if the group is already created)
-        let _: Option<()> = connection
-            .xgroup_create(stream_key, consumer_group, "$")
-            .await
-            .ok();
+        mut connection: MultiplexedConnection,
+        stream_key: &'static str,
+        consumer_group: &'static str,
+        consumer_prefix: &str,
+    ) -> Result<RedisStreamClient, redis::RedisError> {
+        let xgroup_info: StreamInfoGroupsReply = connection.xinfo_groups(stream_key).await?;
+
+        let is_created = xgroup_info
+            .groups
+            .into_iter()
+            .any(|info| info.name == consumer_group);
+
+        if !is_created {
+            let _: () = connection
+                .xgroup_create(stream_key, consumer_group, "$")
+                .await?;
+        }
 
         let stream_consumer_info: redis::streams::StreamInfoConsumersReply = connection
             .xinfo_consumers(stream_key, consumer_group)
@@ -70,7 +89,7 @@ impl<'a, 'b, T: AsyncCommands> RedisStreamClient<'a, 'b, T> {
         })
     }
 
-    pub fn with_no_ack(self) -> RedisStreamClient<'a, 'b, T> {
+    pub fn with_no_ack(self) -> RedisStreamClient {
         Self {
             options: self.options.noack(),
             ..self
@@ -81,9 +100,7 @@ impl<'a, 'b, T: AsyncCommands> RedisStreamClient<'a, 'b, T> {
         &self.consumer_key
     }
 
-    pub async fn read_next(
-        &'a mut self,
-    ) -> Result<Option<RedisStreamMessage<'a, T>>, redis::RedisError> {
+    pub async fn read_next(&mut self) -> Result<Option<RedisStreamMessage>, redis::RedisError> {
         let mut data: StreamReadReply = self
             .connection
             .xread_options(&[self.stream_key], &[">"], &self.options)
@@ -104,7 +121,7 @@ impl<'a, 'b, T: AsyncCommands> RedisStreamClient<'a, 'b, T> {
         Ok(Some(RedisStreamMessage {
             inner_map: msg.map,
             key: msg.id,
-            connection: self.connection,
+            connection: self.connection.clone(),
             group_key: self.consumer_group,
             stream_key: self.stream_key,
         }))
@@ -113,9 +130,30 @@ impl<'a, 'b, T: AsyncCommands> RedisStreamClient<'a, 'b, T> {
 
 #[cfg(test)]
 mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+    use redis::AsyncCommands;
+
+    use crate::RedisStreamClient;
+
+    #[tokio::test]
+    async fn it_works() {
+        let client = redis::Client::open("redis://127.0.0.1/").unwrap();
+
+        let test_stream_key = "test-stream-key";
+        let mut conn = client.get_multiplexed_tokio_connection().await.unwrap();
+        let _: () = conn
+            .xadd(test_stream_key, "*", &[("key", "value")])
+            .await
+            .unwrap();
+
+        let mut stream_client =
+            RedisStreamClient::new(conn, test_stream_key, "test-group", "test-group-consumer")
+                .await
+                .unwrap();
+
+        let msg = stream_client.read_next().await.unwrap().unwrap();
+        println!("{:?}", msg);
+        msg.ack().await.unwrap();
+        let msg = stream_client.read_next().await.unwrap();
+        assert!(msg.is_none())
     }
 }
